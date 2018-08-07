0452babcbeabe19a06acb1e1967decaf0d9082e6
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2018-08-07 23:10:45 +0200

    Adding copy uv script

---
+++ UtilityScripts/CopyUVs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace CustomTools
+{
+	
+	public class CopyUVs : MonoBehaviour {
+
+		public void Copy(){
+			Mesh mesh = GetComponent<MeshFilter> ().sharedMesh;
+			mesh.uv = mesh.uv2;
+		}
+	}
+
+
+	[CustomEditor(typeof(CopyUVs))]
+	public class ObjectBuilderEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			CopyUVs myScript = (CopyUVs)target;
+			if(GUILayout.Button("Copy UVs"))
+			{
+				myScript.Copy();
+			}
+		}
+	}
+}
